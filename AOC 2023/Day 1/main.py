ccce9c96f0b588fcb8f21220d796df3ceb56049e
Avi <93334316+AviNoah@users.noreply.github.com>
2023-12-12 17:56:08 +0200

    Updated part 2 of day 1 2023

--- AOC 2023/Day 1/main.py
+++ AOC 2023/Day 1/main.py
@@ -3,23 +3,44 @@ def main(do_tests: bool = False):
         lines: list = file.readlines()
 
     if do_tests:
-        lines: list = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
+        lines: list = ["1abc2five", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
+
+    numeric_dict = {
+        "zero": 0,
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+        "0": 0,
+        "1": 1,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+    }
 
     def extract_values(line: str) -> int:
         # Extract first and second digits from the line, return as int
-        Min_number_indices: list = [
-            line.index(str(num)) for num in range(10) if str(num) in line
-        ]
-        f_dig = min(Min_number_indices)
-        reversed_line: str = line[::-1]
-        Max_number_indices: list = [
-            reversed_line.index(str(num))
-            for num in range(10)
-            if str(num) in reversed_line
-        ]
-        l_dig = min(Max_number_indices)
+        line: str = line.lower()  # make Lower case
 
-        return int(line[f_dig] + reversed_line[l_dig])
+        # Find indices
+        number_indices: list = [
+            (line.index(dig), val) for dig, val in numeric_dict.items() if dig in line
+        ]
+
+        f_dig = min(number_indices, key=lambda x: x[0])[1]  # Find smallest index
+        l_dig = max(number_indices, key=lambda x: x[0])[1]  # Find largest index
+
+        return f_dig * 10 + l_dig
 
     lines: list = [extract_values(line) for line in lines]
     total = sum(lines)
@@ -27,4 +48,4 @@ def main(do_tests: bool = False):
 
 
 if __name__ == "__main__":
-    main(do_tests=False)
+    main(do_tests=True)
