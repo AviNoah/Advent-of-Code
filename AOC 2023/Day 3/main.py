e468a5123c5ac3059668e24b74abfd1ef362600d
Avi <93334316+AviNoah@users.noreply.github.com>
2023-12-15 00:48:42 +0200

    Implemented part 1 of day 3

--- AOC 2023/Day 3/main.py
+++ AOC 2023/Day 3/main.py
@@ -1,6 +1,21 @@
-def main():
-    pass
+import re
+
+pattern = r"(\d+)"
+
+digits = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+
+
+def make_table():
+    with open("input.txt", "r") as f:
+        lines = f.readlines()
+
+    rows_matches = [re.finditer(pattern, line) for line in lines]
+
+    for i, row in enumerate(rows_matches):
+        for match in row:
+            value = match.group(1)  # Get value
+            print(value)
 
 
 if __name__ == "__main__":
-    main()
+    make_table()
