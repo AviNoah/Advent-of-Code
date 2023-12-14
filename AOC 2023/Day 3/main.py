874d579a79dd88fd5744b9fa795af4e55bcc36a3
Avi <93334316+AviNoah@users.noreply.github.com>
2023-12-15 00:57:18 +0200

    renamed make_table to get_values, also Implemented it

--- AOC 2023/Day 3/main.py
+++ AOC 2023/Day 3/main.py
@@ -2,20 +2,33 @@ import re
 
 pattern = r"(\d+)"
 
-digits = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+non_special = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."}
 
 
-def make_table():
+def get_values() -> list:
     with open("input.txt", "r") as f:
         lines = f.readlines()
 
     rows_matches = [re.finditer(pattern, line) for line in lines]
 
+    results: list = list()
+
     for i, row in enumerate(rows_matches):
         for match in row:
             value = match.group(1)  # Get value
-            print(value)
+            st, ed = match.start(), match.end()
+            # Check if value is surrounded by a symbol that is not a digit or period
+            if all(
+                [
+                    bool(lines[x][y] not in non_special)
+                    for x in range(st - 1, ed + 1)
+                    for y in range(row - 1, row + 1)
+                ]
+            ):
+                results.append(value)
+
+    return results
 
 
 if __name__ == "__main__":
-    make_table()
+    get_values()
