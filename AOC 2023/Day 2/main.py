9abec014bbbf56dcfc4520640fbebabccf643d97
Avi <93334316+AviNoah@users.noreply.github.com>
2023-12-14 22:32:50 +0200

    Added is_bigger_than_bag; implemented might_be_bag fully, should work now

--- AOC 2023/Day 2/main.py
+++ AOC 2023/Day 2/main.py
@@ -1,25 +1,42 @@
 import re
 from functools import reduce
 
-pattern = r"game (\d+):(?:( \d+ (?:blue|red|green))+,*)+;*"
-pattern = re.compile(pattern)
+id_pattern = r"game (\d+)"
+color_pattern = r"(?:( (\d+) (blue|red|green))+,*)+;*"
+id_pattern = re.compile(id_pattern)
+color_pattern = re.compile(color_pattern)
 
 
 def main(bag: dict) -> list:
     with open("input.txt", "r") as f:
         lines: list = f.readlines()
 
+    def is_bigger_than_bag(curr_bag: dict) -> bool:
+        # Return whether curr_bag has the same amount or more balls of each color than bag
+        return all([a >= b for a, b in zip(curr_bag.values(), bag.values())])
+
     def might_be_bag(line: str) -> int:
         # Return the id if the bag in the game might be the given Bag, return None otherwise.
         line: str = line.lower()
-        match = re.match(pattern, line)
 
-        if not match:
-            return None
+        # Each game MUST have id
+        id, data = line.split(":", maxsplit=1)
+        id = re.match(id_pattern, id).group(1)  # Get id
 
-        id = match.group(1)
-        group2 = match.groups()
-        return id
+        # Each game might not necessarily have data
+        matches: list = re.findall(color_pattern, data)
+
+        curr_bag: dict = dict()
+
+        for match in matches:
+            _, val, color = match  # Get value and color, discard string
+            # Initialize to zero if not exists in bag yet, get max
+            val = max(val, curr_bag.get(color, 0))
+            curr_bag[color] = val
+
+        if is_bigger_than_bag(curr_bag):
+            return id
+        return None
 
     result_ids: list = [might_be_bag(line) for line in lines]
     result_ids: list = [id for id in result_ids if id is not None]
