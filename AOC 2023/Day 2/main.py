7255ae10deac282ff9e64bab1178d69447e0041f
Avi <93334316+AviNoah@users.noreply.github.com>
2023-12-14 23:57:47 +0200

    added method get_bags method that retreives bags from input file; added renamed is_maybe_bag to get_bag_from_line; added get_possible_bags to filter games that cant be played with the bag

--- AOC 2023/Day 2/main.py
+++ AOC 2023/Day 2/main.py
@@ -23,23 +23,22 @@ class game_bag:
         values = self.required_bag_to_play.values()
         return reduce(lambda a, b: a * b, values)
 
+    def is_smaller_than_other(self, other_bag: dict) -> bool:
+        # Return whether curr_bag has more or less the same number of balls of each color as bag
+        keys = other_bag.keys()
+        values = [self.get(key, 0) for key in keys]
+        return all([a <= b for a, b in zip(values, other_bag.values())])
+
     def get_bag(self) -> dict:
         return self.required_bag_to_play
 
 
-def get_possible_bags(bag: dict) -> list:
-    # Returns a list of ids of bags that contain at most the same amount of colored balls as in bag
-
+def get_bags() -> list:
+    # Get list of game_bags from file
     with open("input.txt", "r") as f:
         lines: list = f.readlines()
 
-    def is_smaller_than_bag(curr_bag: dict) -> bool:
-        # Return whether curr_bag has more or less the same number of balls of each color as bag
-        keys = bag.keys()
-        values = [curr_bag.get(key, 0) for key in keys]
-        return all([a <= b for a, b in zip(values, bag.values())])
-
-    def can_be_played_with_bag(line: str) -> game_bag:
+    def get_bag_from_line(line: str) -> game_bag:
         # Return the id if the bag in the game might be the given Bag, return None otherwise.
         line: str = line.lower()
 
@@ -58,11 +57,19 @@ def get_possible_bags(bag: dict) -> list:
             val = max(int(val), curr_bag.get(color, 0))
             curr_bag[color] = val
 
-        if is_smaller_than_bag(curr_bag):
-            return game_bag(game_id, curr_bag)
-        return None
+        return game_bag(game_id, curr_bag)
 
-    possible_game_bags: list = [can_be_played_with_bag(line) for line in lines]
+    return [get_bag_from_line(line) for line in lines]
+
+
+def get_possible_bags(bag: dict) -> list:
+    # Returns a list of ids of bags that contain at most the same amount of colored balls as in bag
+
+    game_bags = get_bags()
+
+    possible_game_bags: list = [
+        game_bag for game_bag in game_bags if game_bag.is_smaller_than_other(bag)
+    ]
 
     possible_game_bags: list = [
         game_bag for game_bag in possible_game_bags if game_bag is not None
