ebae84b30f3175e027d32ea87bde1491520cf35e
Avi <93334316+AviNoah@users.noreply.github.com>
2023-12-15 01:11:54 +0200

    renamed row to row_match, added get_value method

--- AOC 2023/Day 3/main.py
+++ AOC 2023/Day 3/main.py
@@ -9,19 +9,24 @@ def get_values() -> list:
     with open("input.txt", "r") as f:
         lines = f.readlines()
 
+    def get_value(row: int, col: int) -> str:
+        if 0 <= row < len(lines) and 0 <= col < len(lines[0]):
+            return lines[row][col]
+        return "."  # This counts as just false
+
     rows_matches = [re.finditer(pattern, line) for line in lines]
 
     results: list = list()
 
-    for i, row in enumerate(rows_matches):
-        for match in row:
+    for i, row_match in enumerate(rows_matches):
+        for match in row_match:
             value: int = int(match.group(1))  # Get value
             st, ed = match.start(), match.end()
             # Check if value is surrounded by a symbol that is not a digit or period
             if any(
                 [
-                    bool(lines[x][y] not in non_special)
-                    for x in range(st - 1, ed + 1)
+                    bool(get_value(y, x) not in non_special)
+                    for x in range(st - 1, ed + 2)
                     for y in range(i - 1, i + 1)
                 ]
             ):
@@ -31,4 +36,5 @@ def get_values() -> list:
 
 
 if __name__ == "__main__":
-    print(sum(get_values()))
+    vals = get_values()
+    print(sum(vals))
