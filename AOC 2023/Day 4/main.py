dc4ab9cdbcc6f67188442d8cb90bb287fa35c0f6
Avi <93334316+AviNoah@users.noreply.github.com>
2023-12-16 01:58:52 +0200

    Fixed logic of count_packs, instead of multiplying lot wins with copies and adding 1 to win_lot next cards, we add 1 to lot_win next cards copies times

--- AOC 2023/Day 4/main.py
+++ AOC 2023/Day 4/main.py
@@ -53,15 +53,14 @@ def count_packs(cards: list[card]) -> int:
     # Use deque for easy and quick insert
     # copy, make a list of winning lots count and count of copies
     copies: list = [1 for _ in cards]
-    result: list = list()
+
     for i, card in enumerate(cards):
         lot_win = len(card.get_winning_lots())
-        lot_win *= copies[i]
         print(f"{copies[i]} copies of card {i+1}")
-
-        for j in range(1, lot_win + 1):
-            index = min(len(copies) - 1, i + j)
-            copies[index] += 1  # Make a copy
+        for _ in range(copies[i]):
+            for j in range(1, lot_win + 1):
+                index = min(len(copies) - 1, i + j)
+                copies[index] += 1  # Make a copy
 
     return sum(copies)
 
