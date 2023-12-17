ce9ea5de6be2f33b960d5bf0a09c04b73b4ff582
Avi <93334316+AviNoah@users.noreply.github.com>
2023-12-17 18:57:42 +0200

    Rewriting solution, implemented new conversion_map class, main differences are that we will use ranges to calculate whether a number is between any of the ranges and do entire conversion process for every number until a better idea is found

---
+++ AOC 2023/Day 5/main2.py
@@ -0,0 +1,34 @@
+class conversion_map:
+    def __init__(
+        self, key_name: str, value_name: str, ranges: list[int, int, int] = list()
+    ):
+        self.key_name: str = key_name
+        self.value_name: str = value_name
+        self.ranges: list[int, int, int] = ranges
+
+    def get_full_name(self) -> str:
+        return f"{self.key_name}-to-{self.value_name}"
+
+    def __str__(self) -> str:
+        # Name
+        result: str = self.get_map_name() + "\n"
+
+        # Padding for key and values
+        padding = len(self.get_map_name())
+
+        # Key value names
+        result += (
+            "{:<{}} {:<{}}".format(
+                str(self.key_name), padding, str(self.value_name), padding
+            )
+            + "\n"
+        )
+
+        # Values
+        for dest_rg, src_rg, rng in self.ranges:
+            padded = "{:<{}} {:<{}} {:<{}}".format(
+                dest_rg, padding, src_rg, padding, rng, padding
+            )
+            result += f"{padded}\n"
+
+        return result
