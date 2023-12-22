e46fb5ef828283a107ee2056231a8978d86f42c1
Avi <93334316+AviNoah@users.noreply.github.com>
2023-12-22 02:09:06 +0200

    Added card_dict

--- AOC 2023/Day 7/main.py
+++ AOC 2023/Day 7/main.py
@@ -1,6 +1,11 @@
+card_types: list = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+card_dict: list = [(type, i) for i, type in enumerate(card_types)]
+card_dict: dict = dict(card_dict)
+
+
 def main():
     return
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
