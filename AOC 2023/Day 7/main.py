ff622fdae68b7ad2732abef70b852dbd11aa37f4
Avi <93334316+AviNoah@users.noreply.github.com>
2023-12-22 02:16:45 +0200

    Added class hand and method count uniques

--- AOC 2023/Day 7/main.py
+++ AOC 2023/Day 7/main.py
@@ -3,6 +3,23 @@ card_dict: list = [(type, i) for i, type in enumerate(card_types)]
 card_dict: dict = dict(card_dict)
 
 
+class hand:
+    def __init__(self, cards: list[str]):
+        self.cards = cards
+
+    def count_uniques(self):
+        # A hand is a list of 5 cards
+        # Return a dictionary that shows how many times each card appears
+        uniques: dict = dict()
+        for card in self.cards:
+            if not card in uniques.keys():
+                uniques[card] = 0  # Initialize
+
+            uniques[card] += 1
+
+        return max(uniques, uniques.values())
+
+
 def main():
     return
 
