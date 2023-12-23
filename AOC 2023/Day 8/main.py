80d364f27e6b1055b21b368e1b755e54edbd3e03
Avi <93334316+AviNoah@users.noreply.github.com>
2023-12-23 22:57:09 +0200

    Added instructions

--- AOC 2023/Day 8/main.py
+++ AOC 2023/Day 8/main.py
@@ -1,3 +1,13 @@
+# First line is input to traverse graph
+# Next lines are the definition of a node network, Node = (Left_child, Right_child)
+# Start at Node AAA, according to the RL inputs, traverse either left or right in every node
+
+# Performing this sequence for a certain number of periods P, will eventually land you on ZZZ
+# So the total steps are P * len(input)
+
+# Find total steps
+
+
 def main():
     return
 
