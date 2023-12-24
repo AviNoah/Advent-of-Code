048da0eb68ba9a675f10cb45ebc374949f7caa67
Avi <93334316+AviNoah@users.noreply.github.com>
2023-12-24 18:08:28 +0200

    Removed old traverse_multiple methods; renamed tr_mult_optimized to traverse_multiple

--- AOC 2023/Day 8/main.py
+++ AOC 2023/Day 8/main.py
@@ -82,45 +82,6 @@ def traverse(directions: str, nodes: dict) -> int:
     return steps
 
 
-def traverse_multiple(directions: str, nodes: dict) -> int:
-    node_objs: list[bi_node] = [nodes[key] for key in nodes.keys() if key[-1] == "A"]
-
-    steps = 0
-
-    dir_len = len(directions)
-
-    periods = 1
-    period_step = 1
-
-    while node_objs:
-        for _ in range(periods):
-            for d in directions:
-                node_objs: list = [
-                    nodes.get(node_obj.get_next(d)) for node_obj in node_objs
-                ]
-
-        old_len = len(node_objs)
-
-        # Remove node_objs that have already reached a Z state
-        node_objs = list(filter(lambda node: node.label[-1] != "Z", node_objs))
-
-        # Period frequency is the amount of times dir len must be stepped until we reach a
-        # state where we have more nodes ending in Z - we can just ignore them in further steps using the periods
-
-        # Add dir_len times the period frequency we are in
-        steps += dir_len * periods
-
-        if len(node_objs) < old_len:
-            dir_len = lcm(periods, dir_len)
-            periods += period_step
-            print(f"Periods is now: {periods}")
-            period_step = 0
-
-        period_step += 1
-
-    return steps
-
-
 def find_period(directions: str, nodes: dict, node: str) -> int:
     # Given an input of R and L turns, return for a given bi_node the first period it reaches a Z state
 
@@ -134,44 +95,7 @@ def find_period(directions: str, nodes: dict, node: str) -> int:
     return period
 
 
-def tr_mult(directions: str, nodes: dict) -> int:
-    node_objs: list[bi_node] = [nodes[key] for key in nodes.keys() if key[-1] == "A"]
-
-    steps = 0
-
-    dir_len = len(directions)
-
-    periods = 0  # We start at 0 periods of dir_len
-    period_step = 0  # We start at 0 period dir_len steps from periods
-
-    while node_objs:
-        for _ in range(max(periods, 1)):
-            for d in directions:
-                node_objs: list = [
-                    nodes.get(node_obj.get_next(d)) for node_obj in node_objs
-                ]
-
-        # Add dir_len * periods
-        steps += dir_len * max(periods, 1)
-
-        # Remove node_objs that have already reached a Z state
-        old_len = len(node_objs)
-        node_objs = list(filter(lambda node: node.label[-1] != "Z", node_objs))
-
-        # Check if length changed:
-        if old_len > len(node_objs):
-            # period_step * periods is the distance from new frequency period to old period.
-            # It took periods + period_step*periods to reach a new frequency where list had shrunk.
-            periods += period_step * max(periods, 1)
-            period_step = 0
-
-        # We entered next period
-        period_step += 1
-
-    return steps
-
-
-def tr_mult_optimized(directions: str, nodes: dict) -> int:
+def traverse_multiple(directions: str, nodes: dict) -> int:
     # We will need to find the periods of all nodes where they first reach a Z state.
     # We will then find the LCM between the results and that will be the total amount of periods
     # dir_len must be traversed, meaning steps will be dir_len * periods
@@ -194,7 +118,7 @@ def part2():
     directions: str = get_directions()
     nodes: dict = get_nodes()
 
-    print(f"Total steps for part 2: {tr_mult_optimized(directions, nodes)}")
+    print(f"Total steps for part 2: {traverse_multiple(directions, nodes)}")
 
 
 def main():
