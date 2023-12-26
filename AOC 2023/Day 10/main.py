b62fa863c0f6d062cde4c57c766abc766913c3f3
Avi <93334316+AviNoah@users.noreply.github.com>
2023-12-26 03:01:30 +0200

    Initialized day 10; added pipe_types dict; added pipe class

---
+++ AOC 2023/Day 10/main.py
@@ -0,0 +1,35 @@
+with open("input.txt", "r") as f:
+    lines: list = f.readlines()
+
+pipe_types: dict = {
+    "|": "north-south",
+    "-": "east-west",
+    "L": "north-east",
+    "J": "north-west",
+    "7": "south-west",
+    "F": "south-east",
+    ".": "ground",
+    "S": "start",
+}
+
+
+class pipe:
+    def __init__(self, symbol: str):
+        ...
+
+
+def part1():
+    pass
+
+
+def part2():
+    pass
+
+
+def main():
+    part1()
+    part2()
+
+
+if __name__ == "__main__":
+    main()
