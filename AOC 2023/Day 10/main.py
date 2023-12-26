50713acfe0369220d8581610648519b14131334e
Avi <93334316+AviNoah@users.noreply.github.com>
2023-12-26 03:08:51 +0200

    Added further instructions

--- AOC 2023/Day 10/main.py
+++ AOC 2023/Day 10/main.py
@@ -1,4 +1,5 @@
 # Find the shape of a pipe, marked S that will create a closed loop with other pipes surrounding it.
+# Afterwards, walk along the loop and find the farthest pipe from it.
 
 with open("input.txt", "r") as f:
     lines: list = f.readlines()
