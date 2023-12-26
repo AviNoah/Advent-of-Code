a1bddf6e938300bd1e589cf6f0b2ff6f38f86d3e
Avi <93334316+AviNoah@users.noreply.github.com>
2023-12-26 02:15:11 +0200

    Added instructions

--- AOC 2023/Day 9/main.py
+++ AOC 2023/Day 9/main.py
@@ -1,7 +1,13 @@
 # Each line is the history of a value
+# The difference between each value will generate a new series, get its difference
+# Repeat until all are zeros.
+# To estimate a new number, we will add a zero, which will add a number to the series above it and so on.
+# This will be called Extrapolation.
+# Sum all extrapolated values.
 
-with open('input.txt', 'r') as f:
+with open("input.txt", "r") as f:
     lines: list = f.readlines()
+
 
 def main():
     return
