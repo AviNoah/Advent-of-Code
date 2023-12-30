5a10b34f6abeb4ff298704c65494a7b984a3ea9b
Avi <93334316+AviNoah@users.noreply.github.com>
2023-12-30 23:56:15 +0200

    Added comments; imported combinatoric ncr from Math module and implemented pair count using it

--- AOC 2023/Day 11/main.py
+++ AOC 2023/Day 11/main.py
@@ -1,5 +1,17 @@
+# Figure out the sum of the lengths of the shortest path between every
+# pair of galaxies
+# Due to something involving gravitational effects, only some space expands.
+# In fact, the result is that any rows or columns that contain no galaxies should
+# all actually be twice as big horizontally and vertically.
+
+from math import comb  # To calculate count of pairs
+
 with open("input.txt", "r") as f:
     lines: list = f.readlines()
+
+
+def pair_count(galaxies: int) -> int:
+    return comb(galaxies, 2)
 
 
 def part1():
