1319b12dedfaefd5ca8f2b3ec8fdc77932224a9b
Avi <93334316+AviNoah@users.noreply.github.com>
2023-12-31 00:30:45 +0200

    implemented pairs_dist

--- AOC 2023/Day 11/main.py
+++ AOC 2023/Day 11/main.py
@@ -32,7 +32,14 @@ class universe:
 
     def pairs_dist(self) -> list:
         # Given a list of galaxies' positions, return the minimal path between each of them
-        ...
+        results = list()
+        for i, galaxy1 in enumerate(self.get_galaxies):
+            for j, galaxy2 in enumerate(self.get_galaxies):
+                if i == j:
+                    continue  # Skip self
+                results.append(universe.min_pair_dist(galaxy1, galaxy2))
+
+        return results
 
     def sum_pair_dists(self) -> int:
         # Return the sum of the minimal distances between galaxies:
