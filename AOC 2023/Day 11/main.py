4242000be75b37b7b58e0a731f60015db4f1769b
Avi <93334316+AviNoah@users.noreply.github.com>
2023-12-31 00:02:36 +0200

    Added min_pair_dist method and pairs_dist

--- AOC 2023/Day 11/main.py
+++ AOC 2023/Day 11/main.py
@@ -10,8 +10,18 @@ with open("input.txt", "r") as f:
     lines: list = f.readlines()
 
 
-def pair_count(galaxies: int) -> int:
+def pair_count(galaxies: tuple[int, int]) -> int:
     return comb(galaxies, 2)
+
+
+def min_pair_dist(galaxy1: tuple[int, int], galaxy2: tuple[int, int]) -> int:
+    # Without going diagonally (you can pass through galaxies).
+    return abs(galaxy2[0] - galaxy1[0]) + abs(galaxy2[1] - galaxy1[1])
+
+
+def pairs_dist(galaxies: list[tuple[int, int]]) -> list:
+    # Given a list of galaxies' positions, return the minimal path between each of them
+    ...
 
 
 def part1():
