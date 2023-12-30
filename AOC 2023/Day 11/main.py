380ab2703cca551ff87d426c69a7b27131ac0c9a
Avi <93334316+AviNoah@users.noreply.github.com>
2023-12-30 23:50:50 +0200

    init day 11

---
+++ AOC 2023/Day 11/main.py
@@ -0,0 +1,19 @@
+with open("input.txt", "r") as f:
+    lines: list = f.readlines()
+
+
+def part1():
+    pass
+
+
+def part2():
+    pass
+
+
+def main():
+    part1()
+    part2()
+
+
+if __name__ == "__main__":
+    main()
