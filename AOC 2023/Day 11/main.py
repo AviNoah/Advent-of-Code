f4bfffad05e273e1d9a757385222d73be204f17f
Avi <93334316+AviNoah@users.noreply.github.com>
2023-12-31 00:09:01 +0200

    Added class universe, moved methods into it; added get_galaxies; pair_count now uses len of get galaxies; pairs_dist now will use get_galaxies; expand will update the self.universe property; min_pair_dist is now a static method

--- AOC 2023/Day 11/main.py
+++ AOC 2023/Day 11/main.py
@@ -10,18 +10,37 @@ with open("input.txt", "r") as f:
     lines: list = f.readlines()
 
 
-def pair_count(galaxies: tuple[int, int]) -> int:
-    return comb(galaxies, 2)
+class universe:
+    def __init__(self, grid) -> None:
+        self.universe: list[list] = grid
 
+    def get_galaxies(self) -> list[tuple[int, int]]:
+        # Return a list of galaxies locations
+        rows = len(self.universe)
+        cols = len(self.universe)
 
-def min_pair_dist(galaxy1: tuple[int, int], galaxy2: tuple[int, int]) -> int:
-    # Without going diagonally (you can pass through galaxies).
-    return abs(galaxy2[0] - galaxy1[0]) + abs(galaxy2[1] - galaxy1[1])
+        result = [
+            (row, col)
+            for col in range(cols)
+            for row in range(rows)
+            if self.universe[row][col] == "#"
+        ]
+        return result
 
+    def pair_count(self) -> int:
+        return comb(len(self.get_galaxies()), 2)
 
-def pairs_dist(galaxies: list[tuple[int, int]]) -> list:
-    # Given a list of galaxies' positions, return the minimal path between each of them
-    ...
+    def pairs_dist(self) -> list:
+        # Given a list of galaxies' positions, return the minimal path between each of them
+        ...
+
+    def expand(self):
+        ...
+
+    @staticmethod
+    def min_pair_dist(galaxy1: tuple[int, int], galaxy2: tuple[int, int]) -> int:
+        # Without going diagonally (you can pass through galaxies).
+        return abs(galaxy2[0] - galaxy1[0]) + abs(galaxy2[1] - galaxy1[1])
 
 
 def part1():
