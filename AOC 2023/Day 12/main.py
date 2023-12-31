f5dfc0378f635c105afff8fedde0b35242f73f2a
Avi <93334316+AviNoah@users.noreply.github.com>
2023-12-31 22:18:41 +0200

    init day 12

---
+++ AOC 2023/Day 12/main.py
@@ -0,0 +1,27 @@
+# Input contains rows of springs, '.' means operational, '#' means damaged, '?' is unknown.
+# immediately after there is a list of the size of each contiguous group of DAMAGED springs
+
+# Equipped with this information, it is your job to figure out how many different
+# arrangements of operational and broken springs fit the given criteria in each row.
+
+# Sum counts of all possible arrangements for every row
+
+with open("input.txt", "r") as f:
+    lines: list = f.readlines()
+
+
+def part1():
+    pass
+
+
+def part2():
+    pass
+
+
+def main():
+    part1()
+    part2()
+
+
+if __name__ == "__main__":
+    main()
