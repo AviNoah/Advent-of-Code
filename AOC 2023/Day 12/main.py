126fe93dc0858af3f95402d93df5208a1363a273
Avi <93334316+AviNoah@users.noreply.github.com>
2023-12-31 23:59:42 +0200

    Added count2 and strategy

--- AOC 2023/Day 12/main.py
+++ AOC 2023/Day 12/main.py
@@ -51,6 +51,7 @@ class spring_row:
             # Check if broken matches is empty or if missing match comes before it
             if not broken_matches or missing_matches[0][0] < broken_matches[0][0]:
                 # Complex case, varying cases, start counting here
+                # TODO: handle what happens if ? is not used to show a broken machine, yet skips to next one
                 index, length = missing_matches.pop(0)
                 # If tmp is less than contiguous[0], set to zero and readd to missing.
                 diff = length - tmp_contiguous[0]
@@ -62,8 +63,9 @@ class spring_row:
 
                 if diff < 0:
                     # Not enough to fill with missing match, give it all.
-                    # Add to count the amount of ways to choose length in tmp_contiguous[0]
-                    count += comb(tmp_contiguous[0], length)
+                    # Add to count of the amount of ways to place length in tmp_contiguous[0]
+                    # The calculation for that is tmp_contiguous[0] - length choose length
+                    count += comb(tmp_contiguous[0] - length, length)
                     tmp_contiguous[0] -= length
 
                 if diff > 0:
@@ -91,6 +93,70 @@ class spring_row:
                 tmp_contiguous.pop(0)
 
         return max(count, 1)  # If no variations found, return 1 at minimum.
+
+    def count2(self) -> int:
+        global broken_pattern, missing_pattern
+        broken_matches: list[re.match] = broken_pattern.finditer(self.operational)
+        missing_matches: list[re.match] = missing_pattern.finditer(self.operational)
+
+        # Extract start index and len from each
+        broken_matches: list[int, int] = [
+            (match.start(), match.end() - match.start()) for match in broken_matches
+        ]
+        missing_matches: list[int, int] = [
+            (match.start(), match.end() - match.start()) for match in missing_matches
+        ]
+
+        # Strategy:
+        """
+        contiguous feed is the first element of the contiguous list
+        while contiguous list is not empty:
+            if there are no missing_matches:
+                rest are broken_matches, break, do not add to count.
+                break from while loop.
+            
+            # There are missing matches
+            if there are broken_matches and broken_match starts before missing match:
+                pop broken match
+                if its length bigger or equal to contiguous feed:
+                    # should never be bigger than.
+                    deduct its length from contiguous feed
+                else:  # It is smaller
+                    # The immediate next missing match MUST start with the rest of the feed.
+                    # Assume there must be a missing match immediately after.
+                    pop missing match, it must begin the sequence, follow three scenarios 
+                    below in else
+                
+            else:
+                pop missing match
+                # We may choose to add or not to add the length of match into the feed,
+                # therefore count of choices increases by 2.
+                choose not to use:
+                    count += 1  # The option to just not used the feed, the 2nd will be added later
+            
+                choose to use:
+                    # We have three scenarios:
+                    - length of match is less than feed:
+                        # Add 1, we use the entire length of match
+                        subtract length of match from feed
+                        count += 1
+                    - length of match is exactly the feed:
+                        set feed to 0
+                        use entire feed
+                    - length of match is more than feed:
+                        add to count number of ways to insert feed into length of match,
+                        which is feed choose length
+                        
+                        # We now split paths, depending on where we choose to insert this feed,
+                        # we create many more possibilities, fill start with working springs
+                        and fill the very next spot after our insert with a working spring since we broke
+                        contiguity.
+                    
+            
+            if contiguous feed is 0:
+                pop it.
+        
+        """
 
     def __str__(self) -> str:
         return (
