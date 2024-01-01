8fe7d52b85c4db6eb876e9aec624d50e8091b71b
Avi <93334316+AviNoah@users.noreply.github.com>
2024-01-01 04:11:04 +0200

    Removed imports, added instructions

--- AOC 2023/Day 12/main1.py
+++ AOC 2023/Day 12/main1.py
@@ -1,6 +1,10 @@
-import re
-from math import comb
+# Input contains rows of springs, '.' means operational, '#' means damaged, '?' is unknown.
+# immediately after there is a list of the size of each contiguous group of DAMAGED springs
 
+# Equipped with this information, it is your job to figure out how many different
+# arrangements of operational and broken springs fit the given criteria in each row.
+
+# Sum counts of all possible arrangements for every row
 
 with open("input.txt", "r") as f:
     lines: list = f.readlines()
