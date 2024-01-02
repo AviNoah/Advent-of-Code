bbaf943cc1ee5d3771ba7229d9edf0803852b8ef
Avi <93334316+AviNoah@users.noreply.github.com>
2024-01-03 00:46:43 +0200

    Added instructions, added class land_data

--- AOC 2023/Day 13/main.py
+++ AOC 2023/Day 13/main.py
@@ -1,5 +1,25 @@
+# Given patterns of rocks . and ash #, figure out where the mirror is.
+# A mirror is either vertical or horizontal, it will reflect the data along it perfectly,
+# it can be placed not in the middle too, meaning one side will have more information than the other,
+# but the side with less information will match the other exactly up until that point.
+# vertical - mirror is exactly between two columns
+# horizontal - mirror is exactly between two rows
+
+# Add number of columns to the left of each vertical mirror, with 100 multiplied by
+# the number of rows above each horizontal mirror.
+
 with open("input.txt", "r") as f:
     lines: list = f.readlines()
+
+
+class land_data:
+    def __init__(self, data: list) -> None:
+        self.data = data
+
+    def get_mirror(self) -> tuple[int, int]:
+        # Return the two columns or two rows representing mirror location
+        # left is lower, right is upper
+        ...
 
 
 def part1():
