a4ffd43facd229d8b451bf5f270d9a0682df834b
Avi <93334316+AviNoah@users.noreply.github.com>
2024-01-05 01:20:38 +0200

    Added instructions, added class stone_grid and methods tilt and calculate_load

--- AOC 2023/Day 14/main.py
+++ AOC 2023/Day 14/main.py
@@ -1,6 +1,29 @@
+# The rounded rocks (O) will roll when the platform is tilted,
+# while the cube-shaped rocks (#) will stay in place, empty spaces are marked (.)
+# You may tilt in all 4 directions
+
+# To calculate the load on a support beam in a direction we do this:
+# The amount of load caused by a single rounded rock (O) is equal to the number of rows 
+# from the rock to the other edge of the platform, including the row the rock is on.
+# square rocks dont add load.
+
+# Find the total load on the north support beam
+
 with open("input.txt", "r") as f:
     lines: list = f.readlines()
 
+
+class stone_grid:
+    def __init__(self, grid) -> None:
+        self.grid = grid
+        
+    def tilt(self, direction):
+        # Move all round rocks as far as you can in the direction given.
+        ...
+        
+    def calculate_load(self, direction):
+        # Calculate load on the beam in the given direction
+        ...
 
 def part1():
     pass
