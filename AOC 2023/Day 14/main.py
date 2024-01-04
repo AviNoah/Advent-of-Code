25ed578c76a46d39b6f862eac1029bf62d8832b4
Avi <93334316+AviNoah@users.noreply.github.com>
2024-01-05 01:21:42 +0200

    updated annotations, added __str__

--- AOC 2023/Day 14/main.py
+++ AOC 2023/Day 14/main.py
@@ -3,27 +3,31 @@
 # You may tilt in all 4 directions
 
 # To calculate the load on a support beam in a direction we do this:
-# The amount of load caused by a single rounded rock (O) is equal to the number of rows 
+# The amount of load caused by a single rounded rock (O) is equal to the number of rows
 # from the rock to the other edge of the platform, including the row the rock is on.
 # square rocks dont add load.
 
 # Find the total load on the north support beam
 
 with open("input.txt", "r") as f:
-    lines: list = f.readlines()
+    lines: list[str] = f.readlines()
 
 
 class stone_grid:
     def __init__(self, grid) -> None:
         self.grid = grid
-        
+
     def tilt(self, direction):
         # Move all round rocks as far as you can in the direction given.
         ...
-        
-    def calculate_load(self, direction):
+
+    def calculate_load(self, direction) -> int:
         # Calculate load on the beam in the given direction
         ...
+
+    def __str__(self) -> str:
+        return "\n".join(self.grid)
+
 
 def part1():
     pass
