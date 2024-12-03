be1df8ae030bc26f6a8477467559656b3d1f7566
Avi Noah <AviNoah2019@gmail.com>
2024-12-03 23:57:19 +0200

    solved part 1

--- AOC 2024/Day 2/main.py
+++ AOC 2024/Day 2/main.py
@@ -6,7 +6,36 @@ data = [[int(item) for item in row] for row in data]
 
 
 def part1():
-    pass
+    def is_safe(row):
+        # Needs to be all increasing or all decreasing
+        # Needs to differ by 1,2 or 3 only between adjacent levels
+        is_ascending = None
+        old = None
+
+        for num in row:
+            if old is None:
+                old = num
+                continue
+            if is_ascending is None:
+                is_ascending = num > old
+
+            diff = num - old
+
+            if (is_ascending and diff < 0) or (not is_ascending and diff > 0):
+                return False
+
+            if not (1 <= abs(diff) <= 3):
+                return False
+
+            old = num
+        return True
+
+    count = 0
+    for row in data:
+        if is_safe(row):
+            count += 1
+
+    return count
 
 
 def part2():
