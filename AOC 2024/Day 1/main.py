009e5ae9889577bfd718b6649841c2b4068ce4a6
Avi Noah <AviNoah2019@gmail.com>
2024-12-03 23:26:57 +0200

    Added tree node

--- AOC 2024/Day 1/main.py
+++ AOC 2024/Day 1/main.py
@@ -1,9 +1,36 @@
+from dataclasses import dataclass
+from typing import Any
+
+
 with open("input.txt", "r") as f:
     lines: list = f.readlines()
 
+    data = [line.strip().split("   ") for line in lines]
+    data = [(int(row[0]), int(row[1])) for row in data]
+
+
+@dataclass
+class TreeNode:
+    value: Any
+    left: "TreeNode" | None = None
+    right: "TreeNode" | None = None
+
+    def insert(self, value: Any):
+        if self.value <= value:
+            if self.left is None:
+                self.left = TreeNode(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = TreeNode(value)
+            else:
+                self.right.insert(value)
+
 
 def part1():
-    pass
+    "We basically have to sort both lists from smallest to largest and pair them"
+    "A good data structure for this is a binary tree"
 
 
 def part2():
