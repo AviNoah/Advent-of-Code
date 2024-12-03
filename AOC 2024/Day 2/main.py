45541ff4f5bc49a6c34382314b53fb026000b783
Avi Noah <AviNoah2019@gmail.com>
2024-12-04 00:30:15 +0200

    started part2

--- AOC 2024/Day 2/main.py
+++ AOC 2024/Day 2/main.py
@@ -39,7 +39,43 @@ def part1():
 
 
 def part2():
-    pass
+
+    # Some examples that can be tolerated
+    # 1 5 6 8
+    # 1 2 7 9
+    # 1 2 2 3
+
+    def is_dist_safe(dist, is_asc):
+        if dist > 0 and not is_asc:
+            return False
+        if not (1 <= abs(dist) <= 3):
+            return False
+        return True
+
+    def is_safe(row):
+        # Needs to be all increasing or all decreasing
+        # Needs to differ by 1,2 or 3 only between adjacent levels
+        # Can have 1 unsafe
+
+        dists = []
+        for i in range(len(row) - 1):
+            dists.append(row[i + 1] - row[i])
+
+        is_asc = dists[0] > 0
+        i = 0
+        while i < len(dists) - 1:
+            if not is_dist_safe(dists[i], is_asc):
+                pass
+
+            i += 1
+        return True
+
+    count = 0
+    for row in data:
+        if is_safe(row):
+            count += 1
+
+    return count
 
 
 def main():
