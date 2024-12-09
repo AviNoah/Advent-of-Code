497946715a3f576cd7388f745f77da57656e561e
Avi Noah <AviNoah2019@gmail.com>
2024-12-09 20:23:27 +0200

    solved part 2

--- AOC 2024/Day 3/main.py
+++ AOC 2024/Day 3/main.py
@@ -1,6 +1,5 @@
 import re
 
-
 with open("input.txt", "r") as f:
     data = f.read()
 
@@ -16,7 +15,19 @@ def part1():
 
 
 def part2():
-    pass
+    global data
+    temp_data = data
+
+    parts = temp_data.split("do()")
+    parts = [part.split("don't()")[0] for part in parts]
+    usable = "".join(parts)
+
+    pattern = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
+    total = 0
+    for match in pattern.finditer(usable):
+        total += int(match.group(1)) * int(match.group(2))
+
+    return total
 
 
 def main():
