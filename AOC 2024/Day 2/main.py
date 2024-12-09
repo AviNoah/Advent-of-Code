4b5417522a687480dbda585044a7787027314b96
Avi Noah <AviNoah2019@gmail.com>
2024-12-09 19:06:35 +0200

    Improved part 1

--- AOC 2024/Day 2/main.py
+++ AOC 2024/Day 2/main.py
@@ -5,31 +5,19 @@ data = [line.strip().split() for line in lines]
 data = [[int(item) for item in row] for row in data]
 
 
+def is_safe(row):
+    diffs = [row[i + 1] - row[i] for i in range(len(row) - 1)]
+    are_asc = [diff > 0 for diff in diffs]
+    within_range = [1 <= abs(diff) <= 3 for diff in diffs]
+
+    same_order = all(are_asc) or not any(are_asc)
+    all_within_range = all(within_range)
+
+    return all_within_range and same_order
+
+
 def part1():
-    def is_safe(row):
-        # Needs to be all increasing or all decreasing
-        # Needs to differ by 1,2 or 3 only between adjacent levels
-        is_ascending = None
-        old = None
-
-        for num in row:
-            if old is None:
-                old = num
-                continue
-            if is_ascending is None:
-                is_ascending = num > old
-
-            diff = num - old
-
-            if (is_ascending and diff < 0) or (not is_ascending and diff > 0):
-                return False
-
-            if not (1 <= abs(diff) <= 3):
-                return False
-
-            old = num
-        return True
-
+    global data
     count = 0
     for row in data:
         if is_safe(row):
@@ -44,48 +32,8 @@ def part2():
     # 1 5 6 8
     # 1 2 7 9
     # 1 2 2 3
-
-    def is_dist_safe(dist, is_asc):
-        if dist > 0 and not is_asc:
-            return False
-        if not (1 <= abs(dist) <= 3):
-            return False
-        return True
-
-    def is_arr_safe(dists: list):
-        is_asc = dists[0] > 0
-        for dist in dists:
-            if not is_dist_safe(dist, is_asc):
-                return False
-
-        return True
-
-    def is_safe(row):
-        # Needs to be all increasing or all decreasing
-        # Needs to differ by 1,2 or 3 only between adjacent levels
-        # Can have 1 unsafe
-
-        dists = []
-        for i in range(len(row) - 1):
-            dists.append(row[i + 1] - row[i])
-
-        is_asc = dists[0] > 0
-        i = 0
-        while i < len(dists) - 1:
-            if not is_dist_safe(dists[i], is_asc):
-                new_arr = dists[:i] + dists[i + 1 :]
-                new_arr[i] += dists[i]
-                return is_arr_safe(new_arr)
-
-            i += 1
-        return True
-
-    count = 0
-    for row in data:
-        if is_safe(row):
-            count += 1
-
-    return count
+    global data
+    pass
 
 
 def main():
