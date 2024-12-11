f3a18862e3e113f42d433ff1909631a382c64421
Avi Noah <AviNoah2019@gmail.com>
2024-12-12 00:04:13 +0200

    began implementing part2

--- AOC 2024/Day 6/main.py
+++ AOC 2024/Day 6/main.py
@@ -62,12 +62,65 @@ def part1():
     return count
 
 
+def would_loop(orig_row, orig_col, orig_facing) -> bool:
+    # TODO: this is not good, what if the loop happens after the block
+    #  +---+  |
+    #  |   |  |
+    #  +---+---
+    #         O <- bloc
+
+    row = orig_row
+    col = orig_col
+    facing = orig_facing
+    FIRST_RUN = True
+    while 0 <= row < ROWS and 0 <= col < COLS:
+        if (
+            not FIRST_RUN
+            and row == orig_row
+            and col == orig_col
+            and facing == orig_facing
+        ):
+            return True
+
+        FIRST_RUN = False
+        row1, col1 = get_next_location(row, col, facing)
+        try:
+            if grid[row1][col1] == "#":
+                facing = rotate_face(facing)
+            else:
+                row, col = row1, col1
+        except IndexError:
+            # We broke out of bounds - guard exited
+            break
+    return False
+
+
 def part2():
-    pass
+    global grid, ROWS, COLS
+    row, col = get_guard_pos(grid)
+    facing = "up"
+    count = 0
+
+    while 0 <= row < ROWS and 0 <= col < COLS:
+        row1, col1 = get_next_location(row, col, facing)
+        try:
+            if grid[row1][col1] == "#":
+                facing = rotate_face(facing)
+            else:
+                # Before continuing, check if it would loop if there would be a block there
+                grid[row1][col1] = "#"
+                if would_loop(row1, col1, rotate_face(facing)):
+                    count += 1
+                grid[row1][col1] = "."
+                row, col = row1, col1
+        except IndexError:
+            # We broke out of bounds - guard exited
+            break
+    return count
 
 
 def main():
-    part1()
+    # part1()
     part2()
 
 
