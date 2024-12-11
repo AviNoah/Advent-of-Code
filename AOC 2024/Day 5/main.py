611ab3043822af3920b9e221df79c749991f076f
Avi Noah <AviNoah2019@gmail.com>
2024-12-11 22:14:40 +0200

    finished part2

--- AOC 2024/Day 5/main.py
+++ AOC 2024/Day 5/main.py
@@ -1,8 +1,11 @@
+from collections import deque
+
+
 with open("input.txt", "r") as f:
     data: str = f.read()
-    rules, updates = data.split("\n\n", maxsplit=1)
+    rules, pages = data.split("\n\n", maxsplit=1)
     rules_lines = rules.split()
-    updates_lines = updates.split()
+    pages_lines = pages.split()
 
     rules_data: dict[str, set[str]] = dict()
     for line in rules_lines:
@@ -11,40 +14,66 @@ with open("input.txt", "r") as f:
             rules_data[a] = {b}
         rules_data[a].add(b)
 
-    updates_data: list[list] = []
-    for line in updates_lines:
+    pages_data: list[list] = []
+    for line in pages_lines:
         items = line.split(",")
-        updates_data.append(items)
+        pages_data.append(items)
+
+
+def is_valid_update(pages: list[str]) -> bool:
+    printed: set[str] = set()
+
+    for page in pages:
+        # check if can print
+        pages_not_to_print = rules_data.get(page, set())
+        if printed.intersection(pages_not_to_print):
+            # bad page, stop
+            return False
+        # good page, print
+        printed.add(page)
+
+    return True
 
 
 def part1() -> None:
-    global rules_data, updates_data
+    global rules_data, pages_data
     correct_middles: list[int] = []
 
-    def is_valid_update(updates: list[str]) -> bool:
-        nonlocal correct_middles
-        printed: set[str] = set()
-
-        for update in updates:
-            # check if can print
-            pages_not_to_print = rules_data.get(update, set())
-            if printed.intersection(pages_not_to_print):
-                # bad page, stop
-                return False
-            # good page, print
-            printed.add(update)
-
-        return True
-
-    for updates in updates_data:
-        if is_valid_update(updates):
-            correct_middles.append(int(updates[len(updates) // 2]))
+    for pages in pages_data:
+        if is_valid_update(pages):
+            correct_middles.append(int(pages[len(pages) // 2]))
 
     total = sum(correct_middles)
     pass
 
 
-def part2():
+def part2() -> None:
+    global pages_data, rules_data
+    invalids = [pages for pages in pages_data if not is_valid_update(pages)]
+
+    def fix_pages(pages: list[str]) -> list[str]:
+        printed_set: set[str] = set()
+        printed_stack: list[str] = []
+
+        for page in pages:
+            bad_pages = rules_data.get(page, set())
+            removed = []
+            while printed_stack and printed_set.intersection(bad_pages):
+                popped = printed_stack.pop()
+                printed_set.remove(popped)
+                removed.append(popped)
+
+            printed_set.add(page)
+            printed_stack.append(page)
+
+            printed_set.update(removed)
+            printed_stack.extend(reversed(removed))
+
+        return printed_stack
+
+    fixed = [fix_pages(pages) for pages in invalids]
+    correct_middles = [int(pages[len(pages) // 2]) for pages in fixed]
+    total = sum(correct_middles)
     pass
 
 
