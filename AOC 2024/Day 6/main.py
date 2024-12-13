7069417a6653a1b4819da430d65d403b7f74040d
Avi Noah <AviNoah2019@gmail.com>
2024-12-14 00:44:51 +0200

    reimplemented stuff

--- AOC 2024/Day 6/main.py
+++ AOC 2024/Day 6/main.py
@@ -6,91 +6,77 @@ with open("input.txt", "r") as f:
 
 
 def get_guard_pos():
-    global grid
     for row in range(ROWS):
         for col in range(COLS):
             if grid[row][col] not in {".", "#"}:
                 return row, col
+    raise
 
 
 GUARD_ROW, GUARD_COL = get_guard_pos()
 
-
-def get_next_location(row, col, facing) -> tuple[int, int]:
-    match facing:
-        case "up":
-            return row - 1, col
-        case "down":
-            return row + 1, col
-        case "right":
-            return row, col + 1
-        case "left":
-            return row, col - 1
-    raise
+ROTATE_RIGHT = {"UP": "RIGHT", "RIGHT": "DOWN", "DOWN": "LEFT", "LEFT": "UP"}
+TO_ADD = {"UP": (-1, 0), "RIGHT": (0, 1), "DOWN": (1, 0), "LEFT": (0, -1)}
 
 
-def rotate_face(facing) -> str:
-    match facing:
-        case "up":
-            return "right"
-        case "down":
-            return "left"
-        case "right":
-            return "down"
-        case "left":
-            return "up"
-    raise
+def is_in_bounds(row, col) -> bool:
+    "Whether or not with in bounds"
+    return 0 <= row < ROWS and 0 <= col < COLS
 
 
-def advance(orig_row, orig_col, facing) -> tuple[int, int, str] | None:
-    "Return the guards next position and direction, if exits, return None"
+def rotate_right(facing):
+    return ROTATE_RIGHT[facing]
 
-    new_row, new_col = get_next_location(orig_row, orig_col, facing)
-    if not (0 <= new_row < ROWS and 0 <= new_col < COLS):
-        return None  # Left bounds
+
+def look_ahead(row, col, facing):
+    r, c = TO_ADD[facing]
+    return row + r, col + c
+
+
+def step(row, col, facing):
+    new_row, new_col = look_ahead(row, col, facing)
+
+    if not is_in_bounds(new_row, new_col):
+        return None
 
     if grid[new_row][new_col] == "#":
-        return orig_row, orig_col, rotate_face(facing)
+        facing = rotate_right(facing)
+    else:
+        row, col = new_row, new_col
 
-    return new_row, new_col, facing
+    return row, col, facing
 
 
 def part1():
-    global grid, ROWS, COLS, GUARD_COL, GUARD_ROW
+    "Advance and mark the grid with X's, if stepped on something that isn't X, count it"
     row, col = GUARD_ROW, GUARD_COL
-    facing = "up"
+    facing = "UP"
 
-    # Guard spawn point is visited
+    count = 1  # guard start pos
     grid[row][col] = "X"
-    count = 1
 
-    while (result := advance(row, col, facing)) is not None:
+    while (result := step(row, col, facing)) is not None:
         row, col, facing = result
-
         if grid[row][col] != "X":
+            grid[row][col] = "X"
             count += 1
-        # Mark that the guard stood there
-        grid[row][col] = "X"
 
     return count
 
 
-def would_loop(orig_row, orig_col, orig_facing) -> bool:
-    "Advance using two guards, one faster than the other, return if they meet again"
-
-    p1: tuple[int, int, str] | None = orig_row, orig_col, orig_facing
-    p2: tuple[int, int, str] | None = orig_row, orig_col, orig_facing
+def does_loop(row, col, facing):
+    "Given guard row col and facing, check if will loop"
+    p1 = row, col, facing
+    p2 = row, col, facing
 
     flag = False
 
     while p1 and p2:
         if flag:
-            # Only advance every two turns
-            p1 = advance(*p1)
+            p1 = step(*p1)
 
-        p2 = advance(*p2)
         flag = not flag
-
+        p2 = step(*p2)
         if p1 == p2:
             return True
 
@@ -98,29 +84,29 @@ def would_loop(orig_row, orig_col, orig_facing) -> bool:
 
 
 def part2():
-    global grid, ROWS, COLS, GUARD_COL, GUARD_ROW
+    "Advance and check every time what if there were a block ahead"
     row, col = GUARD_ROW, GUARD_COL
-    facing = "up"
+    facing = "UP"
 
-    # Needs to be unique
-    blocking_points = set()
+    points = set()
+    bad_points = {(row + r, col + c) for r, c in TO_ADD.values()}
+    bad_points.add((row, col))
 
-    # Must advance once
-    result = advance(row, col, facing)
+    while (result := step(row, col, facing)) is not None:
+        next_row, next_col = look_ahead(row, col, facing)
+        if grid[next_row][next_col] != "#":
+            grid[next_row][next_col] = "#"
 
-    while result is not None:
-        new_row, new_col, new_facing = result
-        if facing == new_facing:
-            # What if it were blocked
-            grid[new_row][new_col] = "#"
-            if would_loop(row, col, rotate_face(facing)):
-                blocking_points.add((new_row, new_col))
-            grid[new_row][new_col] = "."
+            if does_loop(row, col, facing):
+                points.add((next_row, next_col))
+
+            grid[next_row][next_col] = "."
 
         row, col, facing = result
-        result = advance(*result)
 
-    return len(blocking_points)
+    resulting_points = points - bad_points
+    total = len(resulting_points)
+    return total
 
 
 def main():
